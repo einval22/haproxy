b22d50974634f97d6bde136a3fad598809c90205
Valentine Krasnobaeva <vkrasnobaeva@haproxy.com>
2024-10-07 11:53:59 +0200

    MEDIUM: mworker: add support of master recovery mode
    This commit is a part of series to add a support of the 5-th reload
    use case, when the master fails after reload, while loading or parsing
    its new configuration.
    
    Let's slightly refactor here read_cfg_in_discovery_mode() in order to call
    run_master_in_recovery_mode() in cases, when master fails to load or to parse
    its new configuration after relaod. Warning messages are mandatory before
    calling the run_master_in_recovery_mode() as this allows to stop haproxy
    with error, if it was launched in zero-warning mode.
    
    So, in recovery mode, master does not launch any worker. It just performs its
    necessary initialization routines and enters in its polling loop to continue
    to monitor the existed worker process.

--- src/haproxy.c
+++ src/haproxy.c
@@ -3025,23 +3025,45 @@ static void run_master_in_recovery_mode(int argc, char **argv)
 static void read_cfg_in_discovery_mode(int argc, char **argv)
 {
 	struct cfgfile *cfg, *cfg_tmp;
-	int ret;
 
-	/* load configs and read only the keywords with KW_DISCOVERY flag */
+	/* load configs in memory and parse only global section (MODE_DISCOVERY) */
 	global.mode |= MODE_DISCOVERY;
-
+	
 	usermsgs_clr("config");
-	ret = load_cfg(progname);
-	if (ret == 0) {
-		/* read only global section in discovery mode */
-		ret = read_cfg(progname);
+	if (load_cfg(progname) < 0) {
+		if (getenv("HAPROXY_MWORKER_REEXEC") != NULL) {
+			ha_warning("Master failed to load new configuration and "
+				   "can't start a new worker. Already running worker "
+				   "will be kept. Please, check configuration file path "
+				   "and memory limits and reload %s.\n", progname);
+			/* failed to load new conf, so setup master CLI for master side,
+			 * do some init steps and just enter in mworker_loop
+			 * to monitor the existed worker from previous start
+			 */
+			run_master_in_recovery_mode(argc, argv);
+			/* never get there */
+		} else
+			exit(1);
 	}
-	if (ret < 0) {
+
+	if (read_cfg(progname) < 0) {
 		list_for_each_entry_safe(cfg, cfg_tmp, &cfg_cfgfiles, list) {
 			ha_free(&cfg->content);
 			ha_free(&cfg->filename);
 		}
-		exit(1);
+		if (getenv("HAPROXY_MWORKER_REEXEC") != NULL) {
+			ha_warning("Master failed to parse new configuration and "
+				   "can't start a new worker. Already running worker "
+				   "will be kept. Please, check global section settings "
+				   "and memory limits and reload %s.\n", progname);
+			/* failed to load new conf, so setup master CLI for master side,
+			 * do some init steps and just enter in mworker_loop
+			 * to monitor the existed worker from previous start
+			 */
+			run_master_in_recovery_mode(argc, argv);
+			/* never get there */
+		} else
+			exit(1);
 	}
 	usermsgs_clr(NULL);
 
